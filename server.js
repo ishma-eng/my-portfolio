b36d5055469660d2e4c85cfd2d7a2896dc32324b
SIDDIQUE ISHMA IBRAHIM <ishmasiddique@eng.rizvi.edu.in>
2025-05-31 16:58:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Serve static files (CSS, images, etc.)
-app.use(express.static("public"));
+// Serve static files (CSS, images) from root
+app.use(express.static(__dirname));
 
-// Route for homepage
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Route for skills page
 app.get("/skills", (req, res) => {
   res.sendFile(__dirname + "/skills.html");
 });
 
-// Start server
 app.listen(port, () => {
-  console.log("Server started on http://localhost:" + port);
+  console.log(`Server started on http://localhost:${port}`);
 });
 
